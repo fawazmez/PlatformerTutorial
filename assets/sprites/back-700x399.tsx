796630d0806b7bf9ee765b6be7290ac8c025caa8
fawazmez <fawaz.mezher@ocsbstudent.ca>
2018-03-29 12:38:53 -0400

    inti

---
+++ assets/sprites/back-700x399.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="back-700x399" tilewidth="700" tileheight="399" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="700" height="399" source="../../../../../Desktop/back-700x399.jpg"/>
+ </tile>
+</tileset>
